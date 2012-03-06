f45dcc674eb3de9d376ef02a28ac36b6745ddfa7
Michael Zangerle <michael.zangerle@gmail.com>
2012-03-06 12:23:35 +0100

    test 99

--- test/src/gui/GuiClass.java
+++ test/src/gui/GuiClass.java
@@ -14,6 +14,7 @@ public class GuiClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("test");
 		System.out.println("Hello World!");
 	}
 
