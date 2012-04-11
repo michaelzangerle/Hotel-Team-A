e024f14e9b37fd6261c38f5ef7d84f9e89dc749c
Michael Zangerle <michael.zangerle@gmail.com>
2012-04-11 17:58:19 +0200

    hibernate config

---
+++ Project/src/projekt/fhv/teama/hibernate/HibernateHelper.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package projekt.fhv.teama.hibernate;
+
+import javassist.expr.NewArray;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author mike
+ *
+ */
+public class HibernateHelper {
+
+	private static final ThreadLocal session = new ThreadLocal<>();
+	private static final ThreadLocal transaction = new ThreadLocal<>();
+	private static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	
+	
+	public HibernateHelper() {
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Session getSession(){
+		Session session = (Session) HibernateHelper.session.get();
+		if(session == null){
+			session = sessionFactory.openSession();
+			HibernateHelper.session.set(session);
+			}
+		return session;
+	}
+	
+	
+}
