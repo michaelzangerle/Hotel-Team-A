3965e9c2d7ed1b8e18b29d50d78b387a62e0f111
Michael Zangerle <michael.zangerle@gmail.com>
2012-04-18 15:32:38 +0200

    kontodaten leer?

--- Project/src/projekt/fhv/teama/classes/rechnung/Kontodaten.java
+++ Project/src/projekt/fhv/teama/classes/rechnung/Kontodaten.java
@@ -1,65 +1,7 @@
 package projekt.fhv.teama.classes.rechnung;
 
-public class Kontodaten implements IKontodaten {
+public class Kontodaten  {
 
-	@Override
-	public void setId(int id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setIban(String iaban) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getIban() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setBic(String bic) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getBic() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setKontonummer(String ktoNr) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getKontonummer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setBlz(String blz) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getBlz() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// TODO Kontodaten
 
 }
