d5d072e02f563ece85c20e9cf0c014b46653bc5d
Michael Zangerle <michael.zangerle@gmail.com>
2012-04-19 16:51:05 +0200

    rechnungsdao

--- Project/src/projekt/fhv/teama/hibernate/dao/rechnung/IZahlungsmethodeDao.java
+++ Project/src/projekt/fhv/teama/hibernate/dao/rechnung/IZahlungsmethodeDao.java
@@ -2,8 +2,9 @@ package projekt.fhv.teama.hibernate.dao.rechnung;
 
 import projekt.fhv.teama.classes.rechnung.IZahlungsmethode;
 import projekt.fhv.teama.hibernate.dao.IGenericDao;
+import projekt.fhv.teama.hibernate.exceptions.NoDatabaseEntryFoundException;
 
 public interface IZahlungsmethodeDao extends IGenericDao<IZahlungsmethode> {
-	public IZahlungsmethode getZahlungsmethodeByKuerzel(String kz);
-	public IZahlungsmethode getZahlungsmethodeByBezeichnung(String bez);
-}
+	public IZahlungsmethode getZahlungsmethodeByKuerzel(String kz) throws NoDatabaseEntryFoundException;
+	public IZahlungsmethode getZahlungsmethodeByBezeichnung(String bez) throws NoDatabaseEntryFoundException;
+} 
--- Project/src/projekt/fhv/teama/hibernate/dao/rechnung/ZahlungsmethodeDao.java
+++ Project/src/projekt/fhv/teama/hibernate/dao/rechnung/ZahlungsmethodeDao.java
@@ -6,41 +6,75 @@ import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import projekt.fhv.teama.classes.rechnung.IZahlungsmethode;
 import projekt.fhv.teama.classes.rechnung.Zahlungsmethode;
 import projekt.fhv.teama.hibernate.HibernateHelper;
 import projekt.fhv.teama.hibernate.dao.GenericDao;
 import projekt.fhv.teama.hibernate.exceptions.NoDatabaseEntryFoundException;
 
-public class ZahlungsmethodeDao extends GenericDao<Zahlungsmethode> {
+public class ZahlungsmethodeDao extends GenericDao<IZahlungsmethode> implements IZahlungsmethodeDao  {
 
-	public ZahlungsmethodeDao() {
+	private static ZahlungsmethodeDao instance;
+
+	public static IZahlungsmethodeDao getInstance() {
+		if (instance != null) {
+			instance = new ZahlungsmethodeDao();
+		}
+		return instance;
+	}
+
+	private ZahlungsmethodeDao() {
 		super("Zahlungsmethode");
 	}
-	
-	public Zahlungsmethode getZahlungsmethodeByKuerzel(String kuerzel) throws NoDatabaseEntryFoundException {
+
+	public IZahlungsmethode getZahlungsmethodeByKuerzel(String kuerzel) throws NoDatabaseEntryFoundException {
+
+		Zahlungsmethode zahlungsmethode = null;
+
+		try {
+			Session session = HibernateHelper.getSession();
+			Query query = session.createQuery("from " + getTable() + " z where z.kuerzel = :kuerzel");
+			query.setString("kuerzel", kuerzel);
+
+			@SuppressWarnings("rawtypes")
+			List results = query.list();
+
+			if (results.size() == 0) {
+				throw new NoDatabaseEntryFoundException();
+			}
+
+			zahlungsmethode = (Zahlungsmethode) results.get(0);
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+
+		return zahlungsmethode;
+	}
+
+	@Override
+	public IZahlungsmethode getZahlungsmethodeByBezeichnung(String bez) throws NoDatabaseEntryFoundException {
 		
 		Zahlungsmethode zahlungsmethode = null;
 
 		try {
 			Session session = HibernateHelper.getSession();
-			Query query = session.createQuery("from "+getTable()+" z where z.kuerzel = :kuerzel");
-			query.setString("kuerzel", kuerzel);
-			
+			Query query = session.createQuery("from " + getTable() + " z where z.bezeichnung = :bez");
+			query.setString("bez", bez);
+
 			@SuppressWarnings("rawtypes")
 			List results = query.list();
-			
+
 			if (results.size() == 0) {
 				throw new NoDatabaseEntryFoundException();
 			}
-			
+
 			zahlungsmethode = (Zahlungsmethode) results.get(0);
-			
+
 		} catch (HibernateException e) {
 			e.printStackTrace();
 		}
-		
+
 		return zahlungsmethode;
 	}
-	
-
 }
