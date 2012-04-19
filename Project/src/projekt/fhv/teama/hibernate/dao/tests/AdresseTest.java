c4dc1e193cd6bdf8cbaf74cc1ae26d89535d7805
Michael Zangerle <michael.zangerle@gmail.com>
2012-04-19 22:09:25 +0200

    adresse xml

--- Project/src/projekt/fhv/teama/hibernate/dao/tests/AdresseTest.java
+++ Project/src/projekt/fhv/teama/hibernate/dao/tests/AdresseTest.java
@@ -3,6 +3,9 @@ package projekt.fhv.teama.hibernate.dao.tests;
 import projekt.fhv.teama.classes.personen.Adresse;
 import projekt.fhv.teama.hibernate.dao.personen.AdresseDao;
 import projekt.fhv.teama.hibernate.dao.personen.IAdresseDao;
+import projekt.fhv.teama.hibernate.dao.personen.ILandDao;
+import projekt.fhv.teama.hibernate.dao.personen.LandDao;
+import projekt.fhv.teama.hibernate.exceptions.DatabaseException;
 
 /*
  * Testdatum: 17.04
@@ -10,11 +13,13 @@ import projekt.fhv.teama.hibernate.dao.personen.IAdresseDao;
  */
 public class AdresseTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DatabaseException {
 		IAdresseDao adao = AdresseDao.getInstance();
+		ILandDao lnd = LandDao.getInstance();
 		
-		
-		adao.create(new Adresse("Bahnhofstraße 3", "6900", "Bregenz", null));
+		adao.create(new Adresse("Bahnhofstraße 3", "6900", "Bregenz", lnd.getLandByKuerzel("AT")));
+		adao.create(new Adresse("Dorf 46", "6491", "Schönwies", lnd.getLandByKuerzel("AT")));
+		adao.create(new Adresse("Marktstraße 18", "6850", "Dornbirn", lnd.getLandByKuerzel("AT")));
 		
 		
 		// Einfügen
