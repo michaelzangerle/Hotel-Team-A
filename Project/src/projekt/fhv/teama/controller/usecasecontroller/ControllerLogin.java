8a3a5004c5770d7e1aee18efc14955fb1c6f6808
Thomas Gaida <thomas.gaida@students.fhv.at>
2012-04-20 18:51:13 +0200

    ControllerCheckIn

---
+++ Project/src/projekt/fhv/teama/controller/usecasecontroller/ControllerLogin.java
@@ -0,0 +1,18 @@
+package projekt.fhv.teama.controller.usecasecontroller;
+
+import projekt.fhv.teama.controller.interfaces.IControllerMitarbeiter;
+import projekt.fhv.teama.controller.usecasecontroller.interfaces.IControllerLogin;
+
+public class ControllerLogin implements IControllerLogin {
+
+	
+	IControllerMitarbeiter controllermitarbeiter;
+	
+	public ControllerLogin() {
+		
+		
+	}
+	
+	
+	
+}
