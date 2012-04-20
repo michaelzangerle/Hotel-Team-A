f6d9d183a80b5e0c292c92f127e85753c82c0275
jostpatrick <patrick.jost@students.fhv.at>
2012-04-21 00:08:45 +0200

    inc

--- Project/src/projekt/fhv/teama/view/ViewMain.java
+++ Project/src/projekt/fhv/teama/view/ViewMain.java
@@ -17,10 +17,25 @@ public class ViewMain extends Window implements Application, Bindable {
 	Border checkInForm02;	
 	Border checkInForm03;
 	Border checkInForm04;
+	Border occupationPreview;
+		
+	PushButton cf1PBtnNext;
+	PushButton cf2PBtnNext;
+	PushButton cf3PBtnNext;
+	PushButton cf4PBtnNext;
+	
+	PushButton cf1PBtnBack;
+	PushButton cf2PBtnBack;
+	PushButton cf3PBtnBack;
+	PushButton cf4PBtnBack;
 
+	Border progress;
+	Meter meter;
+	
 	BXMLSerializer bxmlSerializer = new BXMLSerializer();	
 	private Window window = null;
-	
+
+		
 	/**
 	 * @param args
 	 */
@@ -64,16 +79,34 @@ public class ViewMain extends Window implements Application, Bindable {
 	public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
 		
 		Label lbReservationDetails = (Label)arg0.get("lbReservationDetails");
-		Border reservationForm01 = (Border)arg0.get("reservationForm01");
+		reservationForm01 = (Border)arg0.get("reservationForm01");
 		checkInForm01 = (Border)arg0.get("checkInForm01");
 		checkInForm02 = (Border)arg0.get("checkInForm02");
 		checkInForm03 = (Border)arg0.get("checkInForm03");
 		checkInForm04 = (Border)arg0.get("checkInForm04");
+		occupationPreview = (Border)arg0.get("occupationPreview");
+		progress = (Border)arg0.get("mainProgress");
+		meter = (Meter)arg0.get("meter");		
 		
-		PushButton cf1PBtnNext = (PushButton)arg0.get("cf1PBtnNext");
-		PushButton cf2PBtnNext = (PushButton)arg0.get("cf2PBtnNext");
-		PushButton cf3PBtnNext = (PushButton)arg0.get("cf3PBtnNext");
-		PushButton cf4PBtnNext = (PushButton)arg0.get("cf4PBtnNext");
+		cf1PBtnNext = (PushButton)arg0.get("cf1PBtnNext");
+		cf2PBtnNext = (PushButton)arg0.get("cf2PBtnNext");
+		cf3PBtnNext = (PushButton)arg0.get("cf3PBtnNext");
+		cf4PBtnNext = (PushButton)arg0.get("cf4PBtnNext");
+		
+		cf2PBtnBack = (PushButton)arg0.get("cf2PBtnBack");
+		cf3PBtnBack = (PushButton)arg0.get("cf3PBtnBack");
+		cf4PBtnBack = (PushButton)arg0.get("cf4PBtnBack");		
+		
+		reservationForm01.setVisible(true);
+		checkInForm01.setVisible(false);
+		checkInForm02.setVisible(false);
+		checkInForm03.setVisible(false);
+		checkInForm04.setVisible(false);
+		occupationPreview.setVisible(false);
+		
+		//progress.setVisible(true);
+		meter.setPercentage(0.25);
+		
 		
 		cf1PBtnNext.getButtonPressListeners().add(new ButtonPressListener()  {
 
@@ -81,7 +114,8 @@ public class ViewMain extends Window implements Application, Bindable {
 			public void buttonPressed(Button arg0) {
 				
 				checkInForm01.setVisible(false);
-				checkInForm02.setVisible(true);				
+				checkInForm02.setVisible(true);
+				meter.setPercentage(meter.getPercentage() + 0.25);
 			}
 			
 		});
@@ -92,19 +126,20 @@ public class ViewMain extends Window implements Application, Bindable {
 			public void buttonPressed(Button arg0) {
 				
 				checkInForm02.setVisible(false);
-				checkInForm03.setVisible(true);				
+				checkInForm03.setVisible(true);
+				meter.setPercentage(meter.getPercentage() + 0.25);
 			}
 			
 		});
-		
-		
+				
 		cf3PBtnNext.getButtonPressListeners().add(new ButtonPressListener()  {
 
 			@Override
 			public void buttonPressed(Button arg0) {
 				
 				checkInForm03.setVisible(false);
-				checkInForm04.setVisible(true);				
+				checkInForm04.setVisible(true);
+				meter.setPercentage(meter.getPercentage() + 0.25);
 			}
 			
 		});
@@ -115,25 +150,53 @@ public class ViewMain extends Window implements Application, Bindable {
 			public void buttonPressed(Button arg0) {
 				
 				checkInForm04.setVisible(false);
-				checkInForm01.setVisible(true);				
+				checkInForm01.setVisible(true);
+				meter.setPercentage(0.25);
+			}
+			
+		});		
+		
+
+		cf2PBtnBack.getButtonPressListeners().add(new ButtonPressListener()  {
+
+			@Override
+			public void buttonPressed(Button arg0) {
+				
+				checkInForm01.setVisible(true);
+				checkInForm02.setVisible(false);
+				meter.setPercentage(meter.getPercentage() - 0.25);
+			}
+			
+		});
+				
+		cf3PBtnBack.getButtonPressListeners().add(new ButtonPressListener()  {
+
+			@Override
+			public void buttonPressed(Button arg0) {
+				
+				checkInForm02.setVisible(true);
+				checkInForm03.setVisible(false);
+				meter.setPercentage(meter.getPercentage() - 0.25);
+			}
+			
+		});
+		
+		cf4PBtnBack.getButtonPressListeners().add(new ButtonPressListener()  {
+
+			@Override
+			public void buttonPressed(Button arg0) {
+				
+				checkInForm03.setVisible(true);
+				checkInForm04.setVisible(false);
+				meter.setPercentage(meter.getPercentage() - 0.25);
 			}
 			
 		});
 		
 		
 		
-		//Window viewMain = (Window)arg0.get("viewMain");
-		
-		reservationForm01.setVisible(false);
-		checkInForm01.setVisible(true);
-		checkInForm02.setVisible(false);
-		checkInForm03.setVisible(false);
-		checkInForm04.setVisible(false);		
 		
 		
-		
-				
-		// lbReservationDetails.setText("Jetzt durch Java gesetzt!");
 	}
 
 }
