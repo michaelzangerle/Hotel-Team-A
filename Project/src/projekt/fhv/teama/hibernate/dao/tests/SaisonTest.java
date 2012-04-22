f6b8ed797d5398a6cd8da37666c04963a0cd6085
Michael Zangerle <michael.zangerle@gmail.com>
2012-04-22 09:46:18 +0200

    zimmerpreis, saison getestet

---
+++ Project/src/projekt/fhv/teama/hibernate/dao/tests/SaisonTest.java
@@ -0,0 +1,62 @@
+package projekt.fhv.teama.hibernate.dao.tests;
+
+import java.sql.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import projekt.fhv.teama.classes.zimmer.ISaison;
+import projekt.fhv.teama.classes.zimmer.Saison;
+import projekt.fhv.teama.hibernate.dao.zimmer.ISaisonDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.SaisonDao;
+import projekt.fhv.teama.hibernate.exceptions.DatabaseException;
+
+public class SaisonTest {
+	
+	public static void main(String[] args) throws DatabaseException {
+		ISaisonDao sDao = SaisonDao.getInstance();
+		
+//		ISaison s1 = new Saison("Frühling", getDate(2012, 3, 1), getDate(2012, 5, 30), null);
+//		ISaison s2 = new Saison("Sommer", getDate(2012, 6, 1), getDate(2012, 8, 30), null);
+//		ISaison s3 = new Saison("Herbst", getDate(2012, 9, 1), getDate(2012, 11, 30), null);
+//		ISaison s4 = new Saison("Winter", getDate(2012, 12, 1), getDate(2012, 2, 30), null);
+//		
+		// Einfügen
+//		sDao.create(s1);
+//		sDao.create(s2);
+//		sDao.create(s3);
+//		sDao.create(s4);
+		
+		// Alle holen
+//		Set<ISaison> saisons = new HashSet<ISaison>(sDao.getAll());
+//		for (ISaison s : saisons) {
+//			System.out.println(s);
+//		}
+		
+
+		// 1 holen
+//		System.out.println(sDao.getById(7));
+		
+		// update
+//		ISaison s1 = sDao.getById(7);
+//		s1.setBezeichnung("Frühling");
+//		sDao.create(s1);
+		
+		// löschen
+//		sDao.remove(sDao.getById(1));
+//		sDao.remove(sDao.getById(2));
+//		sDao.remove(sDao.getById(3));
+//		sDao.remove(sDao.getById(4));
+		
+		// spezial
+//		System.out.println(sDao.getSaison("Frühling"));
+		
+		
+	}
+	
+	private static Date getDate(int year, int month, int day) {
+
+        return new Date(new GregorianCalendar(year, month-1, day).getTime().getTime());
+
+    }
+}
---
+++ Project/src/projekt/fhv/teama/hibernate/dao/tests/ZimmerPreisTest.java
@@ -0,0 +1,73 @@
+package projekt.fhv.teama.hibernate.dao.tests;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import projekt.fhv.teama.classes.zimmer.IKategorie;
+import projekt.fhv.teama.classes.zimmer.ISaison;
+import projekt.fhv.teama.classes.zimmer.IZimmerpreis;
+import projekt.fhv.teama.classes.zimmer.Zimmerpreis;
+import projekt.fhv.teama.hibernate.dao.zimmer.IKategorieDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.ISaisonDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.IZimmerpreisDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.KategorieDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.SaisonDao;
+import projekt.fhv.teama.hibernate.dao.zimmer.ZimmerpreisDao;
+import projekt.fhv.teama.hibernate.exceptions.DatabaseEntryNotFoundException;
+import projekt.fhv.teama.hibernate.exceptions.DatabaseException;
+
+public class ZimmerPreisTest {
+	
+	public static void main(String[] args) throws DatabaseException {
+		
+		IZimmerpreisDao zDao = ZimmerpreisDao.getInstance();
+		ISaisonDao sDao = SaisonDao.getInstance();
+		IKategorieDao kDao = KategorieDao.getInstance();
+		
+//		IZimmerpreis z1 = new Zimmerpreis(sDao.getSaison("Frühling"),kDao.getKategorie("Einzelzimmer"), 35.50F);
+//		IZimmerpreis z2 = new Zimmerpreis(sDao.getSaison("Winter"),kDao.getKategorie("Einzelzimmer"), 38.50F);
+//		IZimmerpreis z3 = new Zimmerpreis(sDao.getSaison("Herbst"),kDao.getKategorie("Einzelzimmer"), 30.50F);
+//		IZimmerpreis z4 = new Zimmerpreis(sDao.getSaison("Sommer"),kDao.getKategorie("Einzelzimmer"), 40.50F);
+//		
+//		IZimmerpreis z5 = new Zimmerpreis(sDao.getSaison("Sommer"),kDao.getKategorie("Doppelzimmer"), 40.50F);
+//		IZimmerpreis z6 = new Zimmerpreis(sDao.getSaison("Herbst"),kDao.getKategorie("Doppelzimmer"), 45.50F);
+//		IZimmerpreis z7 = new Zimmerpreis(sDao.getSaison("Winter"),kDao.getKategorie("Doppelzimmer"), 48.50F);
+//		IZimmerpreis z8 = new Zimmerpreis(sDao.getSaison("Frühling"),kDao.getKategorie("Doppelzimmer"), 42.50F);
+//		
+//		IZimmerpreis z9 = new Zimmerpreis(sDao.getSaison("Sommer"),kDao.getKategorie("Doppelzimmer + Zusatzbett"), 85.50F);
+//		IZimmerpreis z10 = new Zimmerpreis(sDao.getSaison("Herbst"),kDao.getKategorie("Doppelzimmer + Zusatzbett"), 80.50F);
+//		IZimmerpreis z11 = new Zimmerpreis(sDao.getSaison("Frühling"),kDao.getKategorie("Doppelzimmer + Zusatzbett"), 79.50F);
+//		IZimmerpreis z12 = new Zimmerpreis(sDao.getSaison("Winter"),kDao.getKategorie("Doppelzimmer + Zusatzbett"), 75.50F);
+		
+		
+		// Einfügen
+//		zDao.create(z1);
+//		zDao.create(z2);
+//		zDao.create(z3);
+//		zDao.create(z4);
+//		zDao.create(z5);
+//		zDao.create(z6);
+//		zDao.create(z7);
+//		zDao.create(z8);
+//		zDao.create(z9);
+//		zDao.create(z10);
+//		zDao.create(z11);
+//		zDao.create(z12);
+				
+		// Alle holen
+//		Set<IZimmerpreis> preise = new HashSet<IZimmerpreis>(zDao.getAll());
+//		for (IZimmerpreis z : preise) {
+//			System.out.println(z);
+//		}
+		
+		// 1 holen
+//		System.out.println(zDao.getById(5));
+		
+		// update	
+		
+		// löschen
+//		zDao.remove(zDao.getById(5));
+		
+		
+	}
+}
