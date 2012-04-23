30824138008bf5104c0f8b1307c789f39baa3a0d
Thomas Gaida <thomas.gaida@students.fhv.at>
2012-04-23 19:32:21 +0200

    Model Zimmer

--- Project/src/projekt/fhv/teama/model/ModelZimmer.java
+++ Project/src/projekt/fhv/teama/model/ModelZimmer.java
@@ -19,66 +19,68 @@ import projekt.fhv.teama.hibernate.exceptions.DatabaseException;
 import projekt.fhv.teama.model.interfaces.IModelZimmer;
 import projekt.fhv.teama.model.interfaces.IModelZimmerstatus;
 
-
-
 public class ModelZimmer implements IModelZimmer {
-	
 
 	IZimmer zimmerModel;
 	IZimmerDao zimmerDao;
-	
+
 	public ModelZimmer() {
-		zimmerDao=ZimmerDao.getInstance();
+		zimmerDao = ZimmerDao.getInstance();
 	}
 
 	@Override
 	public List<IZimmer> getVerfuegbareZimmerFürGegebeneKategorie(
-			IKategorie ausgewählteKategorie,IReservierung aktulleReservierung) throws DatabaseEntryNotFoundException {
-		
-		List<IZimmer> verfügbare=new Vector<IZimmer>();
+			IKategorie ausgewählteKategorie, IReservierung aktulleReservierung)
+			throws DatabaseEntryNotFoundException {
+
+		List<IZimmer> verfügbare = new Vector<IZimmer>();
 		List<IZimmer> alle = null;
 		try {
 			alle = new Vector<IZimmer>(zimmerDao.getAll());
-			
+
 		} catch (DatabaseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 		for (IZimmer zi : alle) {
-			if(zi.getKategorie().equals(ausgewählteKategorie)&&zi.getZimmerstatus().getKuerzel().equals("FG"))
-			{
-				List<IReservierung> zimmerreserierungen=new Vector<IReservierung>(zi.getReservierungen());
-				for (IReservierung res : zimmerreserierungen) {
-					if(res.getVon().after(aktulleReservierung.getBis())||res.getBis().before(aktulleReservierung.getVon()))
+			if (zi.getKategorie().equals(ausgewählteKategorie)
+					&& zi.getZimmerstatus().getKuerzel().equals("FG")) {
+				List<IReservierung> zimmerreserierungen = new Vector<IReservierung>(
+						zi.getReservierungen());
+				if (zimmerreserierungen.size() > 0) {
+					for (IReservierung res : zimmerreserierungen) {
+						if (res.getVon().after(aktulleReservierung.getBis())
+								|| res.getBis().before(
+										aktulleReservierung.getVon()))
+							verfügbare.add(zi);
+					}
+				} else {
 					verfügbare.add(zi);
 				}
-				
+
 			}
 		}
 		return verfügbare;
-		
+
 	}
 
 	@Override
 	public void save(IZimmer zimmer) {
-		if(zimmer!=null)
+		if (zimmer != null)
 			try {
 				zimmerDao.create(zimmer);
 			} catch (DatabaseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
+
 	}
 
 	@Override
 	public List<IZimmer> getVerfügbareZimmer() {
-		
-		List<IZimmer> verfügbare=new Vector<IZimmer>();
+
+		List<IZimmer> verfügbare = new Vector<IZimmer>();
 		List<IZimmer> alle = null;
 		try {
 			alle = new Vector<IZimmer>(zimmerDao.getAll());
@@ -86,40 +88,33 @@ public class ModelZimmer implements IModelZimmer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		for (IZimmer zi : alle) {
-			if(zi.getZimmerstatus().getKuerzel().equals("fuck"))
-			{
+			if (zi.getZimmerstatus().getKuerzel().equals("fuck")) {
 				verfügbare.add(zi);
 			}
 		}
 		return verfügbare;
 	}
-	
 
 	@Override
 	public void setStatus(IZimmerstatus status) {
 
-		if(status!=null)
-		{
+		if (status != null) {
 			zimmerModel.setZimmerstatus(status);
 		}
-		
+
 	}
 
 	@Override
 	public void setAktullesZimmer(IZimmer zimmer) {
-		zimmerModel=zimmer;
-		
+		zimmerModel = zimmer;
+
 	}
 
 	@Override
 	public IZimmer getAktullesZimmer() {
 		return zimmerModel;
 	}
-	
 
-	
-	
-	
 }
