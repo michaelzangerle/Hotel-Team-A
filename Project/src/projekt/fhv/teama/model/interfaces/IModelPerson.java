4f3765b7efab52e8305d1be843b40da04e0936f3
Thomas Gaida <thomas.gaida@students.fhv.at>
2012-04-24 20:03:06 +0200

    Model Person

--- Project/src/projekt/fhv/teama/model/interfaces/IModelPerson.java
+++ Project/src/projekt/fhv/teama/model/interfaces/IModelPerson.java
@@ -6,33 +6,5 @@ import projekt.fhv.teama.classes.personen.ILand;
 
 public interface IModelPerson {
 	
-	public void setVorname(String vn);
-	public String getVorname();
-	public void setNachname(String nn);
-	public String getNachname();
-	public void setGeschlecht(Character geschlecht);
-	public Character getGeschlecht();
-	public void setStrasse(String strasse);
-	public String getStrasse();
-	public void setOrt(String ort);
-	public String getOrt();
-	public void setPlz(String plz);
-	public String getPlz();
-	public void setGeburtsdatum(Date gebDat);
-	public Date getGeburtsdatum();
-	public void setTelefonnummer(String telNr);
-	public String getTelefonnummer();
-	public void setEmail(String email);
-	public String getEmail();
-	public void setBic(String bic);
-	public String getBic();
-	public void setIban(String iban);
-	public String getIban();
-	public void setKontonummer(String ktoNr);
-	public String getKontonummer();
-	public void setBlz(String blz);
-	public String getBlz();
-	public void setLand(ILand land);
-	public ILand getLand();
 	
 }
