8a3516ce2b03dd2441dad2d04b1e408c90e058a5
jostpatrick <patrick.jost@students.fhv.at>
2012-04-29 02:20:21 +0200

    @BXML statt initialize und unnötige Klassen gelöscht

--- Project/src/projekt/fhv/teama/view/ViewReservation.java
+++ Project/src/projekt/fhv/teama/view/ViewReservation.java
@@ -2,6 +2,7 @@ package projekt.fhv.teama.view;
 
 import java.net.URL;
 
+import org.apache.pivot.beans.BXML;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
@@ -13,24 +14,17 @@ import org.apache.pivot.wtk.PushButton;
 import org.apache.pivot.wtk.TextInput;
 
 public class ViewReservation extends Border implements Bindable {
-	    Form form = null;
-	    PushButton rf1PBtnLoadJava = null;
-	    PushButton clearButton = null;
-	    PushButton rf1PBtnEditReservation = null;
-	    PushButton rf1PBtnDeleteReservation = null;
-	    PushButton rf1PBtnSaveReservation = null;
-	    TextInput rf1TIName = null; 
-	    PushButton cf1PBtnNext = null;
+	    @BXML Form form = null;
+	    @BXML PushButton rf1PBtnLoadJava = null;
+	    @BXML PushButton clearButton = null;
+	    @BXML PushButton rf1PBtnEditReservation = null;
+	    @BXML PushButton rf1PBtnDeleteReservation = null;
+	    @BXML PushButton rf1PBtnSaveReservation = null;
+	    @BXML TextInput rf1TIName = null; 
+	    @BXML PushButton cf1PBtnNext = null;
 	 
 	    @Override
 	    public void initialize(Map<String, Object> namespace, URL location, Resources resources) {
-	        form = (Form)namespace.get("form");
-	        rf1PBtnLoadJava = (PushButton)namespace.get("rf1PBtnLoadJava");
-	        clearButton = (PushButton)namespace.get("clearButton");
-	        rf1PBtnEditReservation = (PushButton)namespace.get("rf1PBtnEditReservation");
-	        rf1PBtnDeleteReservation = (PushButton)namespace.get("rf1PbtnDeleteReservation");
-	        rf1PBtnSaveReservation = (PushButton)namespace.get("rf1PBtnSaveReservation");
-	        rf1TIName = (TextInput) namespace.get("rf1TIName");
 	        
 	        cf1PBtnNext = (PushButton)namespace.get("cf1PBtnNext");
 	        form.setEnabled(false);
